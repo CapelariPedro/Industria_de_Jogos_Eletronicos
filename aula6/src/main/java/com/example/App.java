2826b806643f99fb7d6352ade5af3d2cb11d8cc0
Pedro Vinicius de Morais Capelari <pedro_capelari@hotmail.com>
2025-04-08 20:07:39 -0300

    Commit to main

--- aula6/src/main/java/com/example/App.java
+++ aula6/src/main/java/com/example/App.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package com.example;
 
 import javafx.application.Application;
@@ -26,4 +27,34 @@ public class App extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+=======
+package com.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        //Caminho para o arquivo FXML
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/com/example/login.fxml"));
+        Parent root = loader.load();
+        //Gera o titulo da Página e o Icone
+        stage.setTitle("Flip Niquel");
+        Image image = new Image(this.getClass().getResource("/com/example/imgs/logoFabrica.png").toExternalForm());
+        stage.getIcons().add(image);
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+>>>>>>> Stashed changes
 }
--- aula6/src/main/java/com/example/database/estoque_bd.sql
+++ aula6/src/main/java/com/example/database/estoque_bd.sql
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 CREATE DATABASE estoque_db;
 USE estoque_db;
 
@@ -9,3 +10,38 @@ CREATE TABLE produtos (
     localizacao VARCHAR(100) NOT NULL,
     categoria VARCHAR(100) NOT NULL
 );
+=======
+CREATE DATABASE industria_db;
+USE industria_db;
+
+
+CREATE TABLE estoque_users(
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    senha VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE rh_users(
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    senha VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE produtos (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    quantidade INT NOT NULL,
+    preco DECIMAL(10,2) NOT NULL,
+    localizacao VARCHAR(100) NOT NULL,
+    categoria VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE funcionarios(
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    matricula VARCHAR(30) NOT NULL,
+    cargo VARCHAR(100) NOT NULL,
+    salario VARCHAR(100) NOT NULL,
+    setor VARCHAR(100) NOT NULL
+);
+>>>>>>> Stashed changes
